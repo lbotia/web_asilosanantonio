03808d3ad7ea9e6c746528e1041f20ea95e5c7d9
Laura Botia <meli_botia@hotmail.com>
2017-04-15 23:46:58 -0500

    Se arreglo botones CANCELAR Y AGREGAR en el panel agregar-hermana

--- agregar-hermana.php
+++ agregar-hermana.php
@@ -8,53 +8,93 @@ include_once 'helpers/session.php';
 
 <h5 class="center-align">AGREGAR</h5>
 
+<!-- FORMULARIO DE INSERTAR HERMANA -->
+<div class="container">
+	<div class="row">
+		<div class="card-panel">
+			<form>
 
- <div class="row">
-    <form class="col s12"><div class="row">
-        <div class="input-field col s12">
-          <input id="password" type="password" class="validate">
-          <label for="password">Numero de Cedula</label>
-        </div>
-      </div>
-      <div class="row">
-          <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate">
-          <label for="last_name">Nombres</label>
-        </div>
-        <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate">
-          <label for="last_name">Apellidos</label>
-        </div>
-      </div>
-      
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="password" type="password" class="validate">
-          <label for="password">Lugar de Nacimiento</label>
-        </div>
-      </div>
+				<div class="row">
+					<div class="input-field col s6">
+						<input id="last_name" type="text" class="validate">
+						<label for="last_name">Nombres</label>
+					</div>
+					<div class="input-field col s6">
+						<input id="last_name" type="text" class="validate">
+						<label for="last_name">Apellidos</label>
+					</div>
+				</div>
 
-         <div class="input-field col s12">
-              <input placeholder="FECHA" type="date" class="datepicker">
-              <label for="dob"></label>
-            </div>
-    
-      
-    </form>
-  </div>
-        
-  
+				<div class="row">
+					<div class="input-field col s6">
+						<input id="password" type="password" class="validate">
+						<label for="password">Lugar de Nacimiento</label>
+					</div>
 
+					<div class="input-field col s6">
+						<input placeholder="FECHA" type="date" class="datepicker">
+						<label for="dob"></label>
+					</div>
+				</div>
+
+
+				<div class="row">
+
+					<div class="input-field col s6">
+						<input id="last_name" type="text" class="validate">
+						<label for="last_name">Cedula</label>
+					</div>
+
+					<div class="input-field col s6">
+						<select>
+							<option value="" disabled selected>Seleccione EPS</option>
+							<option value="1">Option 1</option>
+							<option value="2">Option 2</option>
+							<option value="3">Option 3</option>
+						</select>
+						<label>EPS</label>
+					</div>
+
+				</div>
+
+				<div class="row">		
+
+					<div class="col s10">
+						
+							<a class="waves-effect waves-light btn right">CANCELAR</a>
+						
+					</div>
+
+					<div class="col s2">
+						
+							<button class="btn waves-effect waves-light" type="submit" name="action">
+							AGREGAR             			
+             				</button>
+						
+					</div>
+				</div>
+
+			</form>
+		</div>
+	</div>
+</div>
 
 
 
 <?php 	include 'helpers/footer.php';  ?>
 <?php 	include 'helpers/scripts.php';  ?>
- <script>
-         
+<script>
+         $(document).ready(function () {
+            $(".button-collapse").sideNav();
+            $('select').material_select();
+            $("select[required]").css({display: "inline", height: 0, padding: 0, width: 0});           
+           
+        });
          $('.datepicker').pickadate({
               selectMonths: true, // Creates a dropdown to control month
-              selectYears: 70, // Creates a dropdown of 15 years to control year
+              selectYears: 15, // Creates a dropdown of 15 years to control year
               format: 'dd-mm-yyyy' 
             });
-      </script>
+</script>
+
+ 
